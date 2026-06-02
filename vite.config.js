23069427af0f58974c741d89cacd796f2785b8a5
SabrinaSoaresDev <sabrinams9.ss@gmail.com>
2026-06-02 12:23:21 -0300

    buildando

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-// vite.config.js
+// vite.config.js (versão simplificada)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.cjs',
-  },
   server: {
     port: 3000,
   },
-   build: {
-    minify: false, // Desabilita minificação para debug
-    // ou
-    cssMinify: false // Só desabilita minificação de CSS
+  build: {
+    minify: 'esbuild', // ou false
+    cssMinify: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
